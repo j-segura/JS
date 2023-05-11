6c7a6d5ece9fc07b95e4406defd5105b4b719627
j-segura <juanda.segura2005@gmail.com>
2023-05-10 20:11:43 -0500

    validation homework finished

--- validations2/validations.js
+++ validations2/validations.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', () =>{
 
     let formRegister = document.querySelector('#form-register');
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    let passwordConfirm = document.getElementById('passwordConfirm').value;
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let passwordConfirm = document.getElementById('passwordConfirm');
 
     formRegister.addEventListener('submit', (e) => {
 
@@ -20,17 +20,47 @@ window.addEventListener('load', () =>{
         const passwordValue = password.value.trim();
         const passwordConfirmValue = passwordConfirm.value.trim();
 
-        if(!nameValue) {
 
-            console.log('campo nombre en blanco');
-            fail(name, 'Campo nombre vacio');
+        if(!nameValue || /^\s+$/.test(nameValue)) {
+
+            console.log('Name field is empty');
+            fail(name, 'Name field is empty');
 
         }
 
         if(!emailValue) {
 
-            console.log('Campo email vacio');
-            fail(email, 'Campo email vacio')
+            console.log('E-mail field is empty');
+            fail(email, 'E-mail field is empty');
+
+        } else if(!(/\S+@\S+\.\S+/.test(email))) {
+
+            console.log('Invalid e-mail address');
+            fail(email, 'Invalid e-mail address');
+
+        }
+
+        if(!passwordValue || /^\s+$/.test(passwordValue)) {
+
+            console.log('Password field is empty');
+            fail(password, 'Password field is empty');
+
+        } else if (passwordValue.length < 8) {
+
+            console.log('Password needs minimum 8 characters');
+            fail(password, 'Password needs minimum 8 characters');
+
+        }
+
+        if(!passwordConfirmValue) {
+
+            console.log('Confirm password');
+            fail(passwordConfirm, 'Confirm password');
+
+        } else if (passwordConfirmValue !== passwordValue) {
+
+            console.log('Passwords do not match');
+            fail(passwordConfirm, 'Passwords do not match');
 
         }
 
@@ -40,9 +70,10 @@ window.addEventListener('load', () =>{
     const fail = (input, msg) => {
 
         const form = input.parentElement;
-        const notf = form.querySelector('p')
+        const notf = form.querySelector('small')
         notf.innerText = msg;
-        notf.className = 'form-fail'
+        notf.className = 'form-fail';
+        input.className = 'imput-fail';
 
     }
 
